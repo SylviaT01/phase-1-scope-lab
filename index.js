1c0799265951bbafc6e574e19de2b9b65a36e2cf
Sylvia <sylviachebet03@gmail.com>
2024-03-20 00:47:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    window.customerName === 'bob';
+    window.customerName = window.customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'kyle'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'amy'
+}
